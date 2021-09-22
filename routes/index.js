a54862afecd70027ebc159e1b1851a358e471540
Dria Thomas <driathomas96@gmail.com>
2021-09-21 21:39:31 -0400

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
 
 
-=======
-// const express = require("express");
-// const router = express.Router();
-
-
-
-// /* GET home page */
-// router.get("/", (req, res, next) => {
-
-//   res.render("index");
-// });
-
-
-
-// module.exports = router;
-
->>>>>>> 5de1ee95e570f249378ad5ca351a5aaeb6002423
 const router = require("express").Router();
 
 /* GET about us page */
 router.get("/", (req, res, next) => {
-<<<<<<< HEAD
   res.render('', {});
 =======
   res.render('index', {});

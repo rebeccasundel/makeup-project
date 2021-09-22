8323ed96b70afa90bb19027cd7620858d6ba8152
Dria Thomas <76438191+DriaThomas@users.noreply.github.com>
2021-09-21 22:03:09 -0400

    Dria branch (#14)
    * updated signup
    
    * update
    
    * api connected
    
    * routes work
    
    * All Updates
    
    * updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-
-=======
 // const express = require("express");
 // const router = express.Router();
 
@@ -17,16 +14,13 @@
 
 // module.exports = router;
 
->>>>>>> 5de1ee95e570f249378ad5ca351a5aaeb6002423
 const router = require("express").Router();
 
 /* GET about us page */
 router.get("/", (req, res, next) => {
-<<<<<<< HEAD
+
   res.render('', {});
-=======
+
   res.render('index', {});
->>>>>>> 5de1ee95e570f249378ad5ca351a5aaeb6002423
-});
 
 module.exports = router;

0e0eb69d779b5d5e2ee791ba71514bad262c8915
Dria Thomas <driathomas96@gmail.com>
2021-09-23 19:37:01 -0400

    database update

--- routes/collection.routes.js
+++ routes/collection.routes.js
@@ -1,14 +1,84 @@
-const router = require("express").Router();
-const Collection = require('../models/Collection.model')
 
-/* GET about us page */
-router.get("/create", (req, res, next) => {
-    res.render('pages/create');
+// const { default: axios } = require("axios");
+const express = require("express");
+const router = require("express").Router();
+
+const Post = require('../models/Collection.model')
+
+
+
+
+router.get("/create", (req, res) => res.render("pages/create"));
+
+// router.get("/warm", (req, res) => res.render("undertone/warm"));
+
+
+
+// router.get('/create', (req, res) => {
+//     Post.find()
+//         .populate("user")
+//         .then(responseFromDB => {
+//             // console.log(responseFromDB);
+//             res.render('pages/create', { posts: responseFromDB });
+//         })
+//         .catch(err =>
+//             console.log(`Err while getting the books from the  DB: ${err}`)
+//         );
+// });
+
+//post info to database
+router.post("/create", (req, res, next) => {
+    // const productInfo = req.body;
+    Post
+
+        .create(
+            req.body
+        )
+        .then(() => res.redirect(`/collection/create`))
+        .catch((error) => console.log(error));
 });
 
-// router.post('/create', (res, req) => {
-//     res
-// })
+
+router.get("/create", (req, res, next) => {
+    Post.find()
+        .populate("user")
+        .then((responseFromDB) => {
+            const copyOfPost = responseFromDB.map((favPost) => ({
+                brand: favPost.brand,
+                name: favPost.name,
+                products: favPost.products,
+                description: favPost.description,
+                user: room.user.username,
+                user: req.session.user._id.toString() === favPost.user._id.toString(),
+            }));
+            res.render("pages/create", copyOfPost);
+        });
+});
+
+
+
+
+// router.post('/create', (req, res) => {
+//     // console.log(req.body);
+//     const { name, brand, description } = req.body;
+
+//     Post.create({ name, brand, description })
+//         // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
+//         .then(() => res.redirect('/create'))
+//         .catch(error => `Error while creating a new book: ${error}`);
+// });
+
+
+
+
+// router.get("/create", (req, res) => {
+//     // const productInfo = req.body;
+//     Post
+//         .find({}.limit(10)
+
+//             .then((responseFromDB) => res.render(`pages/create`, { posts: responseFromDB });
+
+// });
 
 
 module.exports = router;
--- service/index.js
+++ service/index.js
@@ -8,24 +8,20 @@ const axios = require('axios');
 class ProductsApi {
     constructor() {
         this.api = axios.create({
-            baseURL: 'http://makeup-api.herokuapp.com/api/v1/products.json?brand'
+            baseURL: 'http://makeup-api.herokuapp.com/api/v1/products.json'
         });
     }
 
-    getAllProducts = () => this.api.get('/rankings');
+    getAllProducts = () => this.api.get('/product');
 
-    getOneProduct = productId => this.api.get(`/rankings/${productId}`);
+    getOneProduct = productId => this.api.get(`/product/${productId}`);
 
-    createProduct = productInfo => this.api.post(`/rankings`, productInfo);
+    createProduct = productInfo => this.api.post(`/product`, productInfo);
 
-    editProduct = (productId, productInfo) => this.api.put(`/rankings/${productId}`, productInfo);
+    editProduct = (productId, productInfo) => this.api.put(`/product/${productId}`, productInfo);
 
-    deleteProduct = productId => this.api.delete(`/rankings/${productId}`);
+    deleteProduct = productId => this.api.delete(`/product/${productId}`);
 }
-
-
-
-
 
 
 module.exports = ProductsApi;

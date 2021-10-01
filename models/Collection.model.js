5735bb8109ea78c6d54871383f36055930b3dd2a
Dria Thomas <driathomas96@gmail.com>
2021-09-30 22:09:11 -0400

    update

--- models/Collection.model.js
+++ models/Collection.model.js
@@ -5,13 +5,14 @@ const postSchema = new Schema(
                 brand: { type: String },
                 collectionName: { type: String },
                 name: { type: String },
+                name2: { type: String },
+                name3: { type: String },
                 makeupId: { type: String },
                 description: { type: String },
                 top3: { type: Number },
-                posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-                id: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-                path: [{ type: Array }],
-                user: { type: Schema.Types.ObjectId, ref: "User" },
+                // img: { type: String, enum: ['https://3bc01d2807fb1bc0d25c-a86d2521f1af8989841b9619f5314be5.ssl.cf1.rackcdn.com/products/market-img/rdn-serum-foundation-30-r-30ml.png?v=6ab9fcb8ca2abb9828afcf9dcdad9f94","product_link":"https://deciem.com/product/rdn-serum-foundation-30-r-30ml'] },
+
+
                 undertone: { type: String, enum: ['warm', 'cool', 'neutral'] }
         }
 );

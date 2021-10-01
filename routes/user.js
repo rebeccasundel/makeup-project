8cbc366dee241a570352c6cc6f8e3f56dd0d5c14
Dria Thomas <76438191+DriaThomas@users.noreply.github.com>
2021-10-01 13:18:05 -0400

    Dria branch (#35)
    * updated signup
    
    * update
    
    * api connected
    
    * routes work
    
    * All Updates
    
    * updated routes
    
    * nav updated
    
    * update
    
    * database update
    
    * updated main part
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update edit works
    
    * CRUD done
    
    * update
    
    * upload image
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update cloudinary image upload and more
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,30 @@
 const router = require("express").Router();
 
+
+
 /* GET home page */
+// router.get("/user/profile", isLoggedIn, (req, res, next) => {
+//   console.log(user);
+//   res.render("auth/profile", user);
+// });
+
+// module.exports = router;
 router.get("/profile", (req, res, next) => {
-  console.log(req.session)
-  res.render("index");
+  // console.log(req.session)
+  res.render("auth/profile");
 });
+
+
+// router.post("/profile/:id", (req, res, next) => {
+//   console.log(req.session)
+//     .then(req.session=> {
+//     res.render("auth/profile");
+//   })
+//   .catch (error => console.log("An error occurred while getting a product from database: ", error)); // <--- .catch() - if some error happens handle it here
+// })
+
+// });
+
+
 
 module.exports = router;

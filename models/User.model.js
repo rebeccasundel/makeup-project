7e8dc40213550ad3332522978a35356245b92b34
Dria Thomas <driathomas96@gmail.com>
2021-10-01 23:06:05 -0400

    Adjust the logic for the merror message and put model's props into object

--- models/User.model.js
+++ models/User.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  name: String,
+  name: { type: String },
 
   username: {
     type: String,
@@ -17,7 +17,7 @@ const userSchema = new Schema({
       "Please enter a valid email",
     ],
   },
-  password: String,
+  password: { type: String },
   friendCode: {
     type: String,
     match: [
